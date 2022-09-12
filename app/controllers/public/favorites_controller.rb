510662dcf86d27e34c0190763f2f72104611d78e
oden0304 <108508043+oden0304@users.noreply.github.com>
2022-09-12 10:59:18 +0000

    コメント投稿、いいね非同期修正

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -2,10 +2,12 @@ class Public::FavoritesController < ApplicationController
   def create
     @post_favorite = Favorite.new(user_id: current_user.id, post_id: params[:post_id])
     @post_favorite.save
+    @post = Post.find(params[:post_id])
   end
   
   def destroy
     @post_favorite = Favorite.find_by(user_id: current_user.id, post_id: params[:post_id])
     @post_favorite.destroy
+    @post = Post.find(params[:post_id])
   end
 end
