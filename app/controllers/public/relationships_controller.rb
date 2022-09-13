44a540c48e9f46e28ede26a0d8a97eeec0c42e94
oden0304 <108508043+oden0304@users.noreply.github.com>
2022-09-13 12:25:42 +0000

    フォロー、コメント非同期修正

--- app/controllers/public/relationships_controller.rb
+++ app/controllers/public/relationships_controller.rb
@@ -1,10 +1,12 @@
 class Public::RelationshipsController < ApplicationController
   # フォローするとき
   def create
+    @user = User.find(params[:user_id])
     current_user.follow(params[:user_id])
   end
   # フォロー外すとき
   def destroy
+    @user = User.find(params[:user_id])
     current_user.unfollow(params[:user_id])
   end
   # フォロー一覧
