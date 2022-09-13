4d18e26743dbd168e35cd29335a2115c31eb71ec
oden0304 <108508043+oden0304@users.noreply.github.com>
2022-09-13 01:08:46 +0000

    コメント非同期未修正

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -4,12 +4,10 @@ class Public::CommentsController < ApplicationController
     @comment = current_user.comments.new(comment_params)
     @comment.post_id = @post.id
     @comment.save
-    redirect_to public_post_path(@post)
   end
 
   def destroy
-    Comment.find(params[:id]).destroy
-    redirect_to public_post_path(params[:post_id])
+    Comment.find(params[:post_id]).destroy
   end
   
   private
