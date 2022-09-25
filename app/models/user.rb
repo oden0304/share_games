cff02c88bf2accba450a62d0d3c894424ef620ca
oden0304 <108508043+oden0304@users.noreply.github.com>
2022-09-25 08:13:33 +0000

    名前文字制限追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :authentication_keys => [:name]
          
-  validates :name, presence: true
+  validates :name, presence: true, length: { maximum: 15 }
          
   #新規登録時のチェックボックスのバリデーション       
   validates_acceptance_of :agreement, allow_nil: false, on: :create
