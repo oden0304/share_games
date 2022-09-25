39572990a69554bec779549626f5210f7d226cae
oden0304 <108508043+oden0304@users.noreply.github.com>
2022-09-25 03:05:26 +0000

    ユーザー編集、新規投稿にエラーメッセージ追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,17 +2,9 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller? 
   before_action :authenticate_admin_user!, except: [:top, :terms]
 
-  # before_action :hoge
-  # def hoge
-  #   binding.pry
-  #   sign_in 
-
-  # end
-
   def after_sign_in_path_for(resource)
     case resource
     when Admin
-      # pp "ログイン出来たよ"
       admin_root_path
     when User
       public_user_path(current_user)
@@ -31,7 +23,6 @@ class ApplicationController < ActionController::Base
   end
   
   def authenticate_admin_user!
-    # binding.pry
     if !admin_signed_in? && !user_signed_in? && !devise_controller?
       redirect_to root_path
     end
--- app/models/post.rb
+++ app/models/post.rb
@@ -6,7 +6,7 @@ class Post < ApplicationRecord
   has_many :tags, through: :post_tags, dependent: :destroy
   has_one_attached :post_image
   
-  validates :text, presence: true
+  validates :text, presence: true, length: { maximum: 140 }
   
   def favorited?(user)
     favorites.where(user_id: user.id).exists?
